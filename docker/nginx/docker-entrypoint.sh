8347576dfee6bd311df3160081ce83b6ad871ef8
william <1679826103@qq.com>
2024-09-12 16:02:48 +0800

    env

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -17,8 +17,8 @@ if [ "${NGINX_HTTPS_ENABLED}" = "true" ]; then
     # set the HTTPS_CONFIG environment variable to the content of the https.conf.template
     HTTPS_CONFIG=$(envsubst < /etc/nginx/https.conf.template)
     export HTTPS_CONFIG
-    # Substitute the HTTPS_CONFIG in the default.conf.template with content from https.conf.template
-    envsubst '${HTTPS_CONFIG}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+    # Substitute the HTTPS_CONFIG in the default.conf with content from https.conf.template
+    envsubst '${HTTPS_CONFIG}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 fi
 
 if [ "${NGINX_ENABLE_CERTBOT_CHALLENGE}" = "true" ]; then
@@ -30,10 +30,10 @@ export ACME_CHALLENGE_LOCATION
 
 env_vars=$(printenv | cut -d= -f1 | sed 's/^/$/g' | paste -sd, -)
 
-envsubst "$env_vars" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-envsubst "$env_vars" < /etc/nginx/proxy.conf.template > /etc/nginx/proxy.conf
+envsubst "$env_vars" < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+envsubst "$env_vars" < /etc/nginx/proxy.conf > /etc/nginx/proxy.conf
 
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
 # Start Nginx using the default entrypoint
 exec nginx -g 'daemon off;'
